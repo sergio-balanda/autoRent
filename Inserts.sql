82490e6f5963ba5cbc69e47bf94f4c1f2ce6b589
Rodrigo Emanuel Andreoli <rodrigoandreoli09@gmail.com>
2017-10-25 01:11:22 -0300

    Agrego la base de datos

---
+++ Inserts.sql
@@ -0,0 +1,64 @@
+USE autorent;
+
+INSERT INTO Sucursal (ciudad, provincia, domicilio) VALUES
+('Capital Federal', 'Buenos Aires', 'Jose Bonifacio 846'),
+('Bariloche', 'Rio Negro', 'Rafael Obligado 120'),
+('Cordoba', 'Cordoba', 'Alonso de Ubeda 433'),
+('Iguazu', 'Misiones', 'Aeropuerto'),
+('Rio Grande', 'Tierra del Fuego', 'Aeropuerto');
+
+INSERT INTO Categoria (nombre, costoHora) VALUES
+('4x4', 125.42),
+('Minivan', 97.90),
+('Premium', 86.66),
+('Grande', 77.75),
+('Compacto', 45.50);
+
+INSERT INTO Vehiculo (patente, marca, modelo, capacidadPasajeros, capacidadValijas, kilometraje, fkSucursalV, fkCategoriaV) VALUES
+('AB111CD', 'Toyota', 'Hilux', 5, 6, 0, 4, 1),
+('BC222DE', 'Ford', 'Ranger', 5, 5, 0, 2, 1),
+('CD333EF', 'Chevrolet', 'Spin', 7, 3, 0, 3, 2),
+('DE444FG', 'Mercedes Benz', 'Vito', 8, 2, 0, 1, 2),
+('EF555GH', 'Toyota', 'Corolla', 5, 3, 0, 1, 3),
+('FG666HI', 'Volkswagen', 'Vento', 5, 2, 0, 3, 3),
+('GH777IJ', 'Citroen', 'C4', 5, 2, 0, 3, 4),
+('HI888JK', 'Peugeot', '206 SW', 5, 2, 0, 2, 4),
+('IJ999KL', 'Fortwo', 'Fortwo', 2, 1, 0, 1, 5),
+('JK000LM', 'Volkswagen', 'Up', 4, 2, 0, 2, 5);
+
+INSERT INTO Servicio (nombre, costoPorcentual) VALUES
+('Ninguno', 00.00),
+('Asistencia mecanica', 20.00),
+('Tanque lleno', 10.00),
+('Entrega fuera de hora', 15.00),
+('Entrega en otra sucursal', 15.00),
+('Kilometraje ilimitado', 20.00);
+
+INSERT INTO Accesorio (nombre, costoDia) VALUES
+('Ninguno', 00.00),
+('GPS', 75.00),
+('Telepeaje', 100.00),
+('Porta equipaje', 50.00),
+('Carro para valijas', 50.00),
+('Transporte al aeropuerto', 100.00);
+
+INSERT INTO Cliente (nombre, pasaporte, email, telefono, tarjeta, puntaje) VALUES
+('Krause Sebastian', 'AA123456', 'sebakrause@gmail.com', '541112345678', '1234123412341234', 0),
+('Sarkozy Samantha', 'AA111111', 'ssarkozy@gmail.com', '541199999999', '1111222233334444', 0),
+('Echevarria Joaquin', 'AA222222', 'jo.eche15@gmail.com', '541188888888', '9999888877776666', 0),
+('Ackermann Matias', 'AA333333', 'ackermanm1@gmail.com', '541177777777', '0000222244446666', 0),
+('Corna Gabriela', 'AA444444', 'gabrielacorna99@gmail.com', '541166666666', '3333555577779999', 0);
+
+INSERT INTO Reserva (fkClienteR, fkVehiculoR, fkSucursalR, fechaInicio, fechaFin, costoOrigen) VALUES
+(1, 9, 1, '2018-01-01 09:00:00', '2018-01-01 19:00:00', 455.00), 
+(1, 5, 1, '2018-02-01 09:00:00', '2018-02-06 09:00:00', 10399.20),
+(2, 6, 3, '2018-03-01 09:00:00', '2018-03-02 09:00:00', 2079.84),
+(4, 1, 4, '2018-04-01 09:00:00', '2018-04-01 10:00:00', 125.42),
+(5, 3, 3, '2018-05-01 09:00:00', '2018-05-11 09:00:00', 23496.00);
+
+INSERT INTO Alquiler (fkReservaA, fkServicioA, fkAccesorioA, costoTotal) VALUES 
+(1, 1, 1, 455.00),
+(2, 1, 1, 10399.20),
+(3, 1, 1, 2079.84),
+(4, 1, 1, 125.42),
+(5, 1, 1, 23496.00);
---
+++ Tablas.sql
@@ -0,0 +1,91 @@
+DROP DATABASE IF EXISTS autorent;
+CREATE DATABASE IF NOT EXISTS autorent;
+USE autorent;
+
+CREATE TABLE Sucursal (
+    idSucursal INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    ciudad NVARCHAR(20) NOT NULL,
+    provincia NVARCHAR(20) NOT NULL,
+    domicilio NVARCHAR(40) NOT NULL,
+    PRIMARY KEY (idSucursal)
+);
+
+CREATE TABLE Categoria (
+    idCategoria INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    nombre NVARCHAR(20) NOT NULL,
+    costoHora FLOAT(8, 2) UNSIGNED NOT NULL,
+    PRIMARY KEY (idCategoria)
+);
+
+CREATE TABLE Vehiculo (
+    idVehiculo INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    patente NVARCHAR(7) NOT NULL UNIQUE,
+    marca NVARCHAR(20) NOT NULL,
+    modelo NVARCHAR(20) NOT NULL,
+    capacidadPasajeros TINYINT UNSIGNED NOT NULL,
+    capacidadValijas TINYINT UNSIGNED NOT NULL,
+    kilometraje MEDIUMINT UNSIGNED NOT NULL,
+    fkSucursalV INT UNSIGNED NOT NULL,
+    fkCategoriaV INT UNSIGNED NOT NULL,
+    PRIMARY KEY (idVehiculo),
+    CONSTRAINT fk_sucv FOREIGN KEY (fkSucursalV)
+        REFERENCES Sucursal (idSucursal),
+    CONSTRAINT fk_catv FOREIGN KEY (fkCategoriaV)
+        REFERENCES Categoria (idCategoria)
+);
+
+CREATE TABLE Servicio (
+    idServicio INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    nombre NVARCHAR(50) NOT NULL,
+    costoPorcentual FLOAT(8, 2) UNSIGNED NOT NULL,
+    PRIMARY KEY (idServicio)
+);
+
+CREATE TABLE Accesorio (
+    idAccesorio INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    nombre NVARCHAR(50) NOT NULL,
+    costoDia FLOAT(8, 2) UNSIGNED NOT NULL,
+    PRIMARY KEY (idAccesorio)
+);
+
+CREATE TABLE Cliente (
+    idCliente INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    nombre NVARCHAR(50) NOT NULL,
+    pasaporte NVARCHAR(8) NOT NULL,
+    email NVARCHAR(40) NOT NULL,
+    telefono NVARCHAR(20) NOT NULL,
+    tarjeta NVARCHAR(16) NOT NULL,
+    puntaje MEDIUMINT NOT NULL,
+    PRIMARY KEY (idCliente)
+);
+
+CREATE TABLE Reserva (
+    idReserva INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    fkClienteR INT UNSIGNED NOT NULL,
+    fkVehiculoR INT UNSIGNED NOT NULL,
+    fkSucursalR INT UNSIGNED NOT NULL,
+    fechaInicio DATETIME NOT NULL,
+    fechaFin DATETIME NOT NULL,
+    costoOrigen FLOAT(8, 2) UNSIGNED NOT NULL,
+    PRIMARY KEY (idReserva),
+    CONSTRAINT fk_clir FOREIGN KEY (fkClienteR)
+        REFERENCES Cliente (idCliente),
+    CONSTRAINT fk_vehr FOREIGN KEY (fkVehiculoR)
+        REFERENCES Vehiculo (idVehiculo),
+    CONSTRAINT fk_sucr FOREIGN KEY (fkSucursalR)
+        REFERENCES Sucursal (idSucursal)
+);
+
+CREATE TABLE Alquiler (
+	fkReservaA INT UNSIGNED NOT NULL,
+	fkServicioA INT UNSIGNED NOT NULL,
+	fkAccesorioA INT UNSIGNED NOT NULL,
+    costoTotal FLOAT(8, 2) UNSIGNED NOT NULL,
+    PRIMARY KEY (fkReservaA, fkServicioA, fkAccesorioA),
+    CONSTRAINT fk_resa FOREIGN KEY (fkReservaA)
+        REFERENCES Reserva (idReserva),
+	CONSTRAINT fk_sera FOREIGN KEY (fkServicioA)
+        REFERENCES Servicio (idServicio),
+	CONSTRAINT fk_acca FOREIGN KEY (fkAccesorioA)
+        REFERENCES Accesorio (idAccesorio)
+);
