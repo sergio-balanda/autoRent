6385c80114af602ab1e15b72ff35f5aab3bdeb08
Nicolas Sierra <nicofsierra@gmail.com>
2017-10-31 14:04:24 -0300

    modifique ReservaDaoImpl para que me traiga el idSucursal

--- src/main/java/ar/edu/unlam/tallerweb1/dao/ReservaDaoImpl.java
+++ src/main/java/ar/edu/unlam/tallerweb1/dao/ReservaDaoImpl.java
@@ -2,14 +2,18 @@ package ar.edu.unlam.tallerweb1.dao;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.inject.Inject;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ar.edu.unlam.tallerweb1.modelo.Reserva;
+import ar.edu.unlam.tallerweb1.modelo.Sucursal;
 
 @Service("reservaDao")
 @Transactional
@@ -21,7 +25,7 @@ public class ReservaDaoImpl implements ReservaDao{
 	@Override
 	public void guardarReserva(Integer idVehiculo, String sucursal , String fdsd , String fhst) {
 		final Session session = sessionFactory.getCurrentSession();
-		
+		List<Sucursal> s = new ArrayList<>();
 		 // CAST de String To Date
 		SimpleDateFormat formatodsd = new SimpleDateFormat("yyyy-MM-dd");
 		SimpleDateFormat formatohst = new SimpleDateFormat("yyyy-MM-dd");
@@ -42,9 +46,17 @@ public class ReservaDaoImpl implements ReservaDao{
 		}
 		// -----------FIN CAST
 		
+		//Busco sucursal por ciudad
+		s = session.createCriteria(Sucursal.class)
+					  .add(Restrictions.eq("ciudad",sucursal))
+					  .list();
+		
+		
+		
 		Reserva reserva = new Reserva();
 		reserva.getFkVehiculo().setIdVehiculo(idVehiculo);
-		reserva.getFkSucursalR().setCiudad(sucursal);
+		//de la lista que me devuelve el criteria s me traigo el idSucursal.
+		reserva.getFkSucursalR().setIdSucursal(s.get(0).getIdSucursal());
 		reserva.setFechaInicio(datedsd);
 		reserva.setFechaFin(datehst);
 		reserva.setCostoOrigen(0D);
